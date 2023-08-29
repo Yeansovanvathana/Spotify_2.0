919f297dec376ba423a085410dfc991e1f46d3dc
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-08-29 22:36:02 +0700

    Update icon and title project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Center from "@/components/Center";
 import Player from "@/components/Player";
 import Sidebar from "@/components/Sidebar";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Vathana | Spotify</title>
+        <link rel="icon" href="/icon (2).png" type="image/x-icon" />
+      </Head>
       <main className="flex p-2">
         <Sidebar />
         {/* access token: {x} */}
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { signIn } from "next-auth/react";
+import Head from "next/head";
 
 function login() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+      <Head>
+        <title>Vathana | Spotify</title>
+        <link rel="icon" href="/icon (2).png" type="image/x-icon" />
+      </Head>
       <img
         className="w-52 mb-5 "
         src="https://links.papareact.com/9xl"
